91e34285f7969878b9d2d2cd8eba468fa0ba160e
Addyy16 <147097108+Addyy16@users.noreply.github.com>
2025-09-05 23:12:17 +0530

    Button flash feature added

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
+let gameSeq = [];
+let userSeq = [];
+
+let btns = ["red","yellow", "green", "purple"];
+
 let h2 = document.querySelector('h2');
 let body = document.querySelector('body');
 let level = 0;
 let started = false;
 
-body.addEventListener('keypress', function(){
+document.addEventListener('keypress', function(){
     if(started == false){
-        level++ ;
-        h2.innerText = `Level ${level}`;
         started = true;
+        levelUp();
     }
-})
+});
+
+function btnFlash(btn){
+    btn.classList.add("flash");
+    setTimeout(function(){
+        btn.classList.remove("flash");
+    }, 250);
+}
+
+function levelUp(){
+    level++ ;
+    h2.innerText = `Level ${level}`;
+
+    let randIdx = Math.floor(Math.random()*4);
+    let randColor = btns[randIdx];
+    console.log(randIdx);
+    let randBtn = document.querySelector(`.${randColor}`);
+    btnFlash(randBtn);
+}
